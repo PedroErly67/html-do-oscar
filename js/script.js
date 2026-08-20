195eb7fa121c6c23f9550bf528da68609384a802
PEDRO ERLY PIGGOSSO CORREA <pedro_e_correa@estudante.sesisenai.org.br>
2026-08-19 21:26:17 -0300

    checkpoint 1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
-
-// Get the modal
 var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+
+const user = {
+    "abu" : "1234",
+    "uba" : "4321",
+    "âi" : "1111"
+}
+
+let logar = document.querySelector('#id01 form')
+
+logar.addEventListener('submit', function(ev
+})
